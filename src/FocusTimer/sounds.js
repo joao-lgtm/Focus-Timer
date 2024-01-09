9b9f53ea1152424286f7784400e029c675632b2e
joao.cassatti <joao.cassatti@movemais.com>
2024-01-09 15:44:34 -0200

    first commit

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,3 @@
+export const buttonPressAudio = new Audio('./assets/Chuva.wav')
+export const kitchenTimer = new Audio('./assets/Floresta.wav')
+export const bgAudio = new Audio('./assets/Lareira.wav')
